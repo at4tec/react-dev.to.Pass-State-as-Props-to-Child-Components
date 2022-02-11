94afccc4ef02093d54ba0ab09b7110b2523cff65
at4tec <at4tec@gmail.com>
2022-02-12 00:24:07 +0200

    convert to UCA

--- src/App.js
+++ src/App.js
@@ -3,28 +3,32 @@ import React from "react";
 export default class MyApp extends React.Component {
 
    state = {
-      name: 'CamperBot'
+      items:  {id:'1',name:'ahmed',age:'31'}
     }
   
   render() {
     return (
        <div>
          {/* Change code below this line */}
-         <Navbar name = {this.state.name}/>
+         <Navbar items = {this.state.items}/>
          {/* Change code above this line */}
        </div>
     );
   }
 };
 
+//child
 class Navbar extends React.Component {
 
   render() {
+    const {name,age}=this.props.items
+
     return (
     <div>
       {/* Change code below this line */}
-      <h1>Hello, my name is: {this.props.name}</h1>
+      <h1>Hello, my name is: {name}</h1>
       {/* Change code above this line */}
+      age {age}
     </div>
     );
   }
